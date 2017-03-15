c1468d1863ae28968aa80fa347a89acca8e9bfdd
Ken <kjenney@gmail.com>
2017-03-14 22:18:45 -0400

    Fixed

--- start.sh
+++ start.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 cd /usr/src/app
-#rake db:setup
-echo "Starting migration..."
-rake db:migrate --trace
+rake db:setup
+#echo "Starting migration..."
+#rake db:migrate --trace
 #echo "Starting seeding..."
 #rake db:seed
 ruby app.rb
