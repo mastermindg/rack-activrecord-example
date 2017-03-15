6b143678043b55e3590705c65c2a361a181b6b33
Ken <kjenney@gmail.com>
2017-03-14 20:24:07 -0400

    Fix rake db task

--- app/app.rb
+++ app/app.rb
@@ -4,10 +4,8 @@ require 'json'
 
 puts ENV['hub']
 
-set :database_file, 'config/database.yml'
-
 class Resource < ActiveRecord::Base
   validates :name, presence: true, uniqueness: true
 end
 
-puts json Resource.select('id', 'name').all
+puts JSON Resource.select('id', 'name').all
